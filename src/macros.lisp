85f591d1c4c835f7af1f29710abf679b1d9cb528
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-03 00:53:28 -0200

    Implement the expand-macro method of <cl-ref>

--- src/macros.lisp
+++ src/macros.lisp
@@ -2,7 +2,11 @@
 (defpackage codex.macros
   (:use :cl)
   (:import-from :common-doc
-                :define-node)
+                :<document-link>
+                :<text-node>
+                :define-node
+                :children
+                :text)
   (:import-from :common-doc.macro
                 :<macro-node>
                 :expand-macro))
@@ -14,4 +18,16 @@
   (:documentation "A reference to a Common Lisp symbol."))
 
 (defmethod expand-macro ((ref <cl-ref>))
-  nil)
+  (let ((text-node (elt (children ref) 0)))
+    (assert (typep text-node '<text-node>))
+    (let* ((symbol (text text-node))
+           (colon-pos (position #\: symbol))
+           (package-name (if colon-pos
+                             (subseq symbol 0 colon-pos)))
+           (symbol-name (if colon-pos
+                            (subseq symbol (1+ colon-pos))
+                            symbol)))
+      (make-instance '<document-link>
+                     :document-reference package-name
+                     :section-reference (concatenate 'string
+                                                     "sym-" symbol-name)))))

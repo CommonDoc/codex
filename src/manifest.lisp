eebd460b0beb251e4e4bb6f276902c6c468d8684
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-12 15:56:34 -0200

    Add <config> class

--- src/manifest.lisp
+++ src/manifest.lisp
@@ -1,12 +1,12 @@
 (in-package :cl-user)
 (defpackage codex.manifest
-  (:use :cl)
+  (:use :cl :trivial-types)
   (:documentation "Parsing Codex manifest files."))
 (in-package :codex.manifest)
 
 (defun system-manifest-pathname (system-name)
   "Return the path to a system's manifest file."
-  (merge-pathnames #p"docs/manifest.lisp-expr"
+  (merge-pathnames #p"codex.lisp-expr"
                    (asdf:component-pathname
                     (asdf:find-system system-name))))
 
@@ -15,3 +15,18 @@
   (with-open-file (input-stream pathname
                                 :direction :input)
     (read input-stream)))
+
+(defclass <config> ()
+  ((project-name :reader project-name
+                 :initarg project-name
+                 :type string
+                 :documentation "The project's name.")
+   (markup-format :reader markup-format
+                  :initarg :markup-format
+                  :type keyword
+                  :documentation "The markup format used in docstrings.")
+   (packages :reader packages
+             :initarg :packages
+             :type (proper-list string)
+             :documentation "A list of packages to document."))
+  (:documentation "Documentation options."))

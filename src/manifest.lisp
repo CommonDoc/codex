ab5d6c52590298acabc023b2aef774903e9f7d71
Fernando Borretti <eudoxiahp@gmail.com>
2015-02-01 11:31:44 -0200

    More manifest stuff

--- src/manifest.lisp
+++ src/manifest.lisp
@@ -2,11 +2,25 @@
 (defpackage codex.manifest
   (:use :cl :trivial-types)
   (:documentation "Parsing Codex manifest files."))
-(in-package :codex.manifest)
 
 (defpackage :codex-manifest-user
   (:use :cl :codex.manifest)
   (:documentation "The package in which Codex manifests are read."))
+
+(in-package :codex.manifest)
+
+(defclass document ()
+  ((document-title :reader document-title
+                   :initarg :title
+                   :type string
+                   :documentation "The document's title.")
+   (document-sources :reader document-sources
+                     :initarg :sources
+                     :type (proper-list pathname)
+                     :documentation "A list of pathnames to source files to
+ build the document from."))
+  (:documentation "A Codex document. Project manifests can define multiple
+  documents, e.g. a manual, a tutorial, an advanced manual."))
 
 (defclass manifest ()
   ((project-name :reader project-name
@@ -19,8 +33,12 @@
                   :documentation "The markup format used in docstrings.")
    (packages :reader packages
              :initarg :packages
-             :type (proper-list string)
-             :documentation "A list of packages to document."))
+             :type (proper-list keyword)
+             :documentation "A list of packages to document.")
+   (documents :reader documents
+              :initarg :documents
+              :type (proper-list document)
+              :documentation "A list of documents."))
   (:documentation "Manifest options."))
 
 (defun read-manifest (pathname)
@@ -28,6 +46,19 @@
   (uiop:with-safe-io-syntax (:package (find-package :codex-manifest-user))
     (uiop:read-file-form pathname)))
 
-(defun parse-manifest (pathname)
-  (apply #'make-instance (cons 'manifest (read-manifest pathname))))
+(defun parse-document (document-plist)
+  "Parse a manifest's document plist into a document object."
+  (apply 'make-instance (cons 'document document-plist)))
 
+(defun parse-manifest (pathname)
+  "Parse a manifest from a pathname."
+  (let ((plist (read-manifest pathname)))
+    (destructuring-bind
+        (&key project-name markup-format packages documents)
+        plist
+      (make-instance 'manifest
+                     :project-name project-name
+                     :markup-format markup-format
+                     :packages packages
+                     :documents (loop for doc in documents collecting
+                                  (parse-document doc))))))

665ad63b53867b44a9d5ae677da24879894c6c98
Fernando Borretti <eudoxiahp@gmail.com>
2015-02-15 19:39:52 -0200

    Add some docs, written in Codex itself

---
+++ docs/manifest.lisp
@@ -0,0 +1,6 @@
+(:project-name "Codex"
+ :markup-format :vertex
+ :systems (:codex)
+ :documents ((:title "Codex Documentation"
+              :authors ("Fernando Borretti")
+              :sources ("overview.tex"))))

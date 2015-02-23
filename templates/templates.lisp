fd5c43c8d87ff796a5ea4a369be29fd5d631374c
Fernando Borretti <eudoxiahp@gmail.com>
2015-02-23 07:51:11 -0300

    More reasonable templates

--- templates/templates.lisp
+++ templates/templates.lisp
@@ -1,14 +1,20 @@
 (in-package :cl-user)
 (defpackage codex.tmpl
   (:use :cl)
+  (:import-from :common-html.template
+   :template
+   :render
   (:documentation "Codex template definitions."))
 (in-package :codex.tmpl)
+
+;;; Djula templates
 
 (djula:add-template-directory
  (asdf:system-relative-pathname :codex #p"templates/"))
 
-(defparameter +base+
-  (djula:compile-template* "base.html"))
-
 (defparameter +min-section+
   (djula:compile-template* "min/section.html"))
+
+(defparameter +min-doc+
+  (djula:compile-template* "min/document.html"))
+

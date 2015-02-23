489853c9b982fa987e46bed084668acb8a926755
Fernando Borretti <eudoxiahp@gmail.com>
2015-02-23 16:34:52 -0300

    Add a couple of errors

--- src/error.lisp
+++ src/error.lisp
@@ -1,4 +1,3 @@
-
 (in-package :codex.error)
 
 (define-condition codex-error ()
@@ -25,3 +24,24 @@
      (format stream "Unsupported markup format: ~A." (format-name condition))))
   (:documentation "Signalled when Codex doesn't know the markup format a
   manifest specifies."))
+
+(define-condition unsupported-output-format (codex-error)
+  ((format-name :reader format-name
+                :initarg :format-name
+                :type keyword
+                :documentation "The name of the output format."))
+  (:report
+   (lambda (condition stream)
+     (format stream "Unsupported output format: ~A." (format-name condition))))
+  (:documentation "Signalled when Codex doesn't know the output format a
+  document specifies."))
+
+(define-condition unknown-template (codex-error)
+  ((template-name :reader template-name
+                  :initarg :template-name
+                  :type keyword
+                  :documentation "The template name."))
+  (:report
+   (lambda (condition stream)
+     (format stream "Unknown template: ~A." (format-name condition))))
+  (:documentation "Signalled when the user provides a template that's not present in the template database."))

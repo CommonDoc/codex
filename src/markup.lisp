7bda67f858c54c0a424b559017d1a7fda4ab75c6
Fernando Borretti <eudoxiahp@gmail.com>
2015-05-03 09:09:56 -0300

    Use Pandocl for parsing

--- src/markup.lisp
+++ src/markup.lisp
@@ -1,29 +1,19 @@
+(defpackage codex.markup
+  (:use :cl)
+  (:export :with-markup
+           :parse-string)
+  (:documentation "Parsing files and docstrings."))
 (in-package :codex.markup)
 
-(defparameter +known-markup-formats+
-  (let ((table (make-hash-table)))
-    (setf (gethash :vertex table) (make-instance 'vertex:vertex))
-    table)
-  "A hash table of the known markup formats.")
-
-(defun validate-markup-format (format-name)
-  "Returns t if format-name is known, signals unsupported-markup-format
-otherwise."
-  (if (gethash format-name +known-markup-formats+)
-      t
-      (error 'codex.error:unsupported-markup-format :format-name format-name)))
-
-(defvar *current-markup-format* :vertex
-  "The markup format that will be used to parse docstrings and files.")
+(defvar *current-markup-format* nil
+  "The name of the markup format that will be used to parse docstrings and
+ files. This is a keyword that is passed to Pandocl.")
 
 (defmacro with-markup ((format-name) &rest body)
   "Execute body with a markup format."
-  `(when (validate-markup-format ,format-name)
-     (let ((*current-markup-format* ,format-name))
-       ,@body)))
+  `(let ((*current-markup-format* ,format-name))
+     ,@body))
 
 (defun parse-string (string)
-  "Parse a docstring or file string into a documentation node."
-  (common-doc.format:parse-document
-   (gethash *current-markup-format* +known-markup-formats+)
-   string))
+  "Parse a docstring into a documentation node using the current markup format."
+  (pandocl:parse-string string *current-markup-format*))

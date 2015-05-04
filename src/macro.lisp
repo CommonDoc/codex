b8a6cbe18ca133ac4ba4898391e466e10e6596af
Fernando Borretti <eudoxiahp@gmail.com>
2015-05-04 19:55:21 -0300

    Reimplement documentation node insertion

--- src/macro.lisp
+++ src/macro.lisp
@@ -15,7 +15,6 @@
                 :make-text
                 :make-meta)
   (:export :*index*
-           :cl-doc
            :param)
   (:documentation "CommonDoc macros for Codex."))
 (in-package :codex.macro)
@@ -25,16 +24,27 @@
 (defvar *index* nil
   "The current Docparser index.")
 
+(defvar *current-package-name* nil
+  "The name of the current package. Set by the with-package macro.")
+
 ;;; Macro classes
 
 (define-node cl-doc (macro-node)
   ()
-  (:tag-name "cldoc")
-  (:documentation "Insert parsed documentation."))
+  (:tag-name "cl:doc")
+  (:documentation "Insert documentation of a node."))
+
+(define-node with-package (macro-node)
+  ((name :reader package-macro-name
+         :type string
+         :attribute-name "name"
+         :documentation "The package's name."))
+  (:tag-name "cl:with-package")
+  (:documentation "Set the current package to use in the body."))
 
 (define-node param (macro-node)
   ()
-  (:tag-name "param")
+  (:tag-name "cl:param")
   (:documentation "An argument of an operator."))
 
 ;;; Utilities
@@ -48,14 +58,6 @@
                       (concatenate 'string
                                    "codex-"
                                    class))))))
-
-(defun parse-symbol-string (string)
-  "Parse a symbol string into a list of two values: Package name (string) and
-symbol name (string)."
-  (let* ((colon-pos (position #\: string))
-         (package-name (subseq string 0 colon-pos))
-         (symbol-name (subseq string (1+ colon-pos))))
-    (list package-name symbol-name)))
 
 ;;; Docparser nodes to CommonDoc nodes
 
@@ -83,12 +85,13 @@ symbol name (string)."
                                             (make-text
                                              (with-standard-io-syntax
                                                (let ((*print-case* :downcase))
-                                                 (prin1-to-string
+                                                 (princ-to-string
                                                   (docparser:operator-lambda-list node)))))))
                   (make-instance 'content-node
                                  :metadata (make-class-metadata "docstring")
-                                 :children (list
-                                            (docparser:node-docstring node))))))
+                                 :children (children
+                                            (codex.markup:parse-string
+                                             (docparser:node-docstring node)))))))
 
 (defmethod expand-node ((node docparser:function-node))
   "Expand a function node."
@@ -119,31 +122,82 @@ create an error message."
 
 ;;; Macroexpansions
 
-(defmethod expand-macro ((cl-doc cl-doc))
-  (let ((text-node (elt (children cl-doc) 0)))
-    (assert (typep text-node 'text-node))
-    (destructuring-bind (package symbol)
-        (parse-symbol-string (text text-node))
-      (format t "Inserting documentation for symbol ~S.~%" symbol)
-      ;; Extract the node from the index
-      (let ((nodes (docparser:query *index*
-                                    :package-name (string-upcase package)
-                                    :symbol-name (string-upcase symbol))))
-        (if (> (length nodes) 0)
-            (expand-node (elt nodes 0))
-            ;; No node with that name, report an error
-            (make-instance 'content-node
-                           :metadata (make-class-metadata (list "error" "no-node"))
-                           :children
-                           (list
-                            (make-text "No node with name ")
-                            (make-instance 'code
-                                           :children
-                                           (list
-                                            (make-text (string-downcase symbol))))
-                            (make-text "."))))))))
+(defparameter +type-name-to-class-map+
+  (list (cons "function"  'docparser:function-node)
+        (cons "macro"     'docparser:macro-node)
+        (cons "generic"   'docparser:generic-function-node)
+        (cons "method"    'docparser:method-node)
+        (cons "variable"  'docparser:variable-node)
+        (cons "struct"    'docparser:struct-node)
+        (cons "class"     'docparser:class-node)
+        (cons "type"      'docparser:type-node)
+        (cons "cfunction" 'docparser:cffi-function)
+        (cons "ctype"     'docparser:cffi-type)
+        (cons "cstruct"   'docparser:cffi-struct)
+        (cons "cunion"    'docparser:cffi-union)
+        (cons "cenum"     'docparser:cffi-enum)
+        (cons "cbitfield" 'docparser:cffi-bitfield))
+  "Associate the string names of Docparser classes to the corresponding
+docparser class names.")
 
-(defmethod expand-macro ((param param))
+(defun find-node-type-by-name (name)
+  (rest (assoc name +type-name-to-class-map+ :test #'equal)))
+
+(defun node-not-found (symbol)
   (make-instance 'content-node
+                 :metadata (make-class-metadata (list "error" "no-node"))
+                 :children
+                 (list
+                  (make-text "No node with name ")
+                  (make-instance 'code
+                                 :children
+                                 (list
+                                  (make-text (string-downcase symbol))))
+                  (make-text "."))))
+
+(defun no-such-type (name)
+  (make-instance 'content-node
+                 :metadata (make-class-metadata (list "error" "no-type"))
+                 :children
+                 (list
+                  (make-text "No node type with name ")
+                  (make-instance 'code
+                                 :children
+                                 (list (make-text name)))
+                  (make-text "."))))
+
+(defun find-node (type symbol rest)
+  (let ((class (find-node-type-by-name type)))
+    (if class
+        (let ((nodes (docparser:query *index*
+                                      :package-name *current-package-name*
+                                      :symbol-name (string-upcase symbol)
+                                      :class class)))
+          (if (> (length nodes) 0)
+              (expand-node (elt nodes 0))
+              ;; No node with that name, report an error
+              (node-not-found symbol)))
+        (no-such-type type))))
+
+(defmethod expand-macro ((node cl-doc))
+  (let ((text-node (elt (children node) 0)))
+    (assert (typep text-node 'text-node))
+    (let* ((arguments (split-sequence:split-sequence #\Space (text text-node))))
+      (destructuring-bind (type symbol &rest rest)
+          arguments
+        (format t "Inserting documentation for ~A ~S.~%" type symbol)
+        (find-node type symbol rest)))))
+
+(defmethod expand-macro ((node with-package))
+  (let* ((package-name (package-macro-name node))
+         (*current-package-name* (string-upcase package-name))
+         (new-node (make-instance 'content-node
+                                  :children (children node))))
+    ;; Expand inner macros
+    (common-doc.macro:expand-macros new-node)
+    new-node))
+
+(defmethod expand-macro ((node param))
+  (make-instance 'code
                  :metadata (make-class-metadata "param")
-                 :children (children param)))
+                 :children (children node)))

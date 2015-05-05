8b7cfd0fcd28d5802384598016426e70f8f8d46c
Fernando Borretti <eudoxiahp@gmail.com>
2015-05-05 18:45:47 -0300

    More tests

--- t/codex.lisp
+++ t/codex.lisp
@@ -3,11 +3,50 @@
   (:use :cl :fiveam))
 (in-package :codex-test)
 
+;;; Constants
+
+(defparameter +doc-build-directory+
+  (asdf:system-relative-pathname :codex-test-system
+                                 #p"docs/build/"))
+
+;;; Tests
+
 (def-suite tests)
 (in-suite tests)
+
+(test set-up
+  ;; Ensure the test system's docs build directory is empty
+  (finishes
+    (when (probe-file +doc-build-directory+)
+      (uiop:delete-directory-tree +doc-build-directory+
+                                  :validate t))))
 
 (test document-test-system
   (finishes
    (codex:document :codex-test-system)))
+
+(test files-exist
+  (let ((files (list #p"html/section-a.html"
+                     #p"html/static/style.css"
+                     #p"html/static/highlight.js"
+                     #p"html/static/highlight.css")))
+    (loop for file in files do
+      (is-true
+       (probe-file (merge-pathnames file
+                                    +doc-build-directory+))))))
+
+(test macros-work
+  ;; Ensure all the names are right
+  (is-false
+   (search "No node with name"
+           (uiop:read-file-string
+            (merge-pathnames  #p"html/section-a.html"
+                              +doc-build-directory+))))
+  ;; Ensure all nodes are supported
+  (is-false
+   (search "Unsupported node type"
+           (uiop:read-file-string
+            (merge-pathnames  #p"html/section-a.html"
+                              +doc-build-directory+)))))
 
 (run! 'tests)
--- t/test-system/test-system.lisp
+++ t/test-system/test-system.lisp
@@ -7,10 +7,57 @@
 (defparameter var t
   "docstring")
 
+(defvar var2 t
+  "docstring")
+
+(defconstant const t
+  "docstring")
+
 (defun func (a b &optional (c "") (l 1))
   "docstring"
+  (declare (ignore a b c l))
   t)
 
 (defmacro mac (a b c)
   "docstring"
+  (declare (ignore a b c))
   t)
+
+(defstruct struct
+  "docstring"
+  a
+  (b "test" :type string)
+  (c 1))
+
+(deftype custom-string (val)
+  "docstring"
+  (string= val "my-string"))
+
+(defclass test-class ()
+  ((first-slot :accessor first-slot
+               :initarg :first-slot
+               :documentation "docstring")
+   (second-slot :reader second-slot
+                :reader s-slot
+                :initarg :second-slot
+                :documentation "docstring")
+   (unexported-slot :reader unexported-slot
+                    :initarg :unexported-slot
+                    :documentation "docstring"))
+  (:documentation "docstring"))
+
+(defgeneric test-method (obj a)
+  (:documentation "docstring"))
+
+(defmethod test-method ((tc test-class) a)
+  "docstring"
+  (declare (ignore tc a))
+  t)
+
+(defmacro indirectly-define-function ()
+  "docstring"
+  `(defun hidden-function ()
+     "docstring"
+     t))
+
+(indirectly-define-function)

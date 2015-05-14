77363458817589d2a24c92047baf71f6a7505f1d
Fernando Borretti <eudoxiahp@gmail.com>
2015-05-14 20:00:20 -0300

    Remove unused code

--- src/markup.lisp
+++ src/markup.lisp
@@ -9,11 +9,6 @@
   "The name of the markup format that will be used to parse docstrings and
  files. This is a keyword that is passed to Pandocl.")
 
-(defmacro with-markup ((format-name) &rest body)
-  "Execute body with a markup format."
-  `(let ((*current-markup-format* ,format-name))
-     ,@body))
-
 (defun parse-string (string)
   "Parse a docstring into a documentation node using the current markup format."
   (pandocl:parse-string string *current-markup-format*))

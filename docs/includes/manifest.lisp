87a640abc04d652ba521765496457b66ff4bb142
Fernando Borretti <eudoxiahp@gmail.com>
2015-07-29 21:15:20 -0300

    Add some documentation includes

---
+++ docs/includes/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:my-library)
+ :documents ((:title "My Library"
+              :authors ("John Q. Lisper")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))

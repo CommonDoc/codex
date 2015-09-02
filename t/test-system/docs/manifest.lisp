343aa7dcea59e4646bf19203258ce725b0d6d83f
Fernando Borretti <eudoxiahp@gmail.com>
2015-09-01 23:01:20 -0300

    Remove a document

--- t/test-system/docs/manifest.lisp
+++ t/test-system/docs/manifest.lisp
@@ -9,12 +9,6 @@
              (:title "Doc B"
               :authors ("Fernando Borretti")
               :output-format (:type :multi-html
-                              :template :traditional)
-              :sources ("sec-a.scr"
-                        "sec-b.scr"))
-             (:title "Doc C"
-              :authors ("Fernando Borretti")
-              :output-format (:type :multi-html
                               :template :gamma)
               :sources ("sec-a.scr"
                         "sec-b.scr"))))
